51ee2d007f991b3f4dfaad477293dc46fb6b1802
Assem Ali <assem.omar.ali@gmail.com.com>
2025-11-02 19:19:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# NextDetail
+
+Static site project. 
+
+How to run locally:
+
+1. Open `index.html` (or use Live Server in VS Code)
+
+Notes:
+- This repository was initialized locally and linked to https://github.com/assemali3585/NextDetail.git
---
+++ scripts/header.js
@@ -0,0 +1,12 @@
+header.innerHTML = `<div class="header">
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo">
+        </div>
+        <div class="menu">
+            <div>
+                <a href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact</a>
+                </div>
+            </div>
+    </div>`;
